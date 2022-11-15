0aa0e54c9c11bd1e3c8c5af0041e570373603534
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2022-11-14 23:56:23 -0300

    ajustes

--- steam/src/screens/Login/styles.js
+++ steam/src/screens/Login/styles.js
@@ -10,8 +10,6 @@ export const Container = styled.View`
 export const Title = styled.Text`
   width: 163px;
   height: 59px;
-  left: 113px;
-  top: 24px;
   font-family: sans-serif;
   font-style: normal;
   font-weight: 300;
@@ -19,40 +17,37 @@ export const Title = styled.Text`
   line-height: 53px;
   color: #ffffff;
   text-align: center;
+  margin-top: -250px;
+  margin-bottom: 30px;
 `;
 
 export const InputText = styled.Text`
-width: 183px;
-height: 21px;
-left: 40px;
-top: 123px;
 font-family: sans-serif;
 font-style: normal;
 font-weight: 400;
 font-size: 16px;
 line-height: 21px;
 color: #FFFFFF;
-text-transform: capitalize;
+text-transform: uppercase;
+margin-bottom: 10px;
 `;
 
 export const InputLogin = styled.TextInput`
 width: 327px;
 height: 71px;
-left: 31px;
-top: 157px;
 
 background: #202126;
 border-radius: 10px;
+margin-bottom: 25px;
 `;
 
 export const InputPassword = styled.TextInput`
 width: 327px;
 height: 71px;
-left: 31px;
-top: 157px;
 
 background: #202126;
 border-radius: 10px;
+margin-bottom: 25px;
 `;
 
 export const Recover = styled.Text`
@@ -72,24 +67,20 @@ color: #FFFFFF;
 export const StButton = styled.TouchableOpacity`
 width: 327px;
 height: 63px;
-left: 31px;
-top: 379px;
-
-background: #226DAE;
+background: #226DAE; 
 border-radius: 10px;
+margin-bottom: 40px;
+align-items: center;
+justify-content: center;
 `;
 
 export const TextButton = styled.Text`
+position: absolute;
+text-align: center;
+color: #FFFFFF;
 width: 76px;
 height: 32px;
-left: 156px;
-top: 390px;
-
-font-family: sans-serif;
-font-style: normal;
-font-weight: 400;
 font-size: 24px;
-line-height: 32px;
-
-color: #FFFFFF;
+font-weight: 400;
+font-family: sans-serif;
 `;
