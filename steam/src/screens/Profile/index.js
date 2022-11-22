f19950789eb9c76096f932c2b72e6a4f43406416
Grazi Falk <83370784+grazifalk@users.noreply.github.com>
2022-11-22 15:57:14 -0300

    Avanços na página de usuário

--- steam/src/screens/Profile/index.js
+++ steam/src/screens/Profile/index.js
@@ -1,36 +1,83 @@
 import { StatusBar } from "expo-status-bar";
 import { Header } from "../../components/Header";
-import { Category, Container, Country, EditButton, LastView, Level, Name, Service, Status, TextButton, Title, Top, User } from "./styles";
+import {
+  Category,
+  Container,
+  Country,
+  EditButton,
+  LastView,
+  Level,
+  Name,
+  Service,
+  Status,
+  TextButton,
+  User,
+  ViewStatus,
+  ViewUser,
+  ViewName,
+  ButtonUser,
+  CountryIcon,
+  ViewImage,
+  UserImage,
+  ViewUserDetails,
+  ViewUserComplete,
+  ViewButton
+} from "./styles";
 import { useNavigation } from "@react-navigation/native";
 import { Menu } from "../../components/Menu";
+import { AntDesign } from "@expo/vector-icons";
+import colors from "../../themes/colors";
 
 export default function Profile() {
   const navigation = useNavigation();
 
   function goBack() {
     navigation.goBack();
-  };
-  
+  }
+
   return (
-      <Container>
-        <Header title={"community"} iconName={"ios-chevron-back"} goBack={goBack}  />
+    <Container>
+      <Header
+        title={"community"}
+        iconName={"ios-chevron-back"}
+        goBack={goBack}
+      />
+      <ViewUser>
         <User>missfalk</User>
+        <ButtonUser>
+          <AntDesign name="caretdown" size={12} color={colors.tertiary} />
+        </ButtonUser>
+      </ViewUser>
+      <ViewName>
         <Name>Grazi Falk</Name>
+        <CountryIcon source={require("../../../assets/brazil.svg")} />
         <Country>Brazil</Country>
-        <Level>Level</Level>
-        <Service>Years of Service</Service>
-        <Service>600 XP</Service>
-        <EditButton>
+      </ViewName>
+      <ViewUserComplete>
+      <ViewImage>
+      <UserImage source={require("../../../assets/user.jpg")} />
+      </ViewImage>
+      <ViewUserDetails>
+      <Level>Level</Level>
+      <Service>Years of Service</Service>
+      <Service>600 XP</Service>
+      </ViewUserDetails>
+      </ViewUserComplete>
+      <ViewButton>
+      <EditButton>
         <TextButton>Edit Profile</TextButton>
-        </EditButton>
+      </EditButton>
+      </ViewButton>
+      <ViewStatus>
         <Status>Currently Offline</Status>
         <LastView>Last Online 47 days ago</LastView>
-        <Category>Badges</Category>
-        <Category>Groups</Category>
-        <Category>Friends</Category>
-        <Category>Games</Category>
-        <Menu />
-        <StatusBar style="auto" />
-      </Container>
+      </ViewStatus>
+      <Category>Badges</Category>
+      <Category>Groups</Category>
+      <Category>Friends</Category>
+      <Category>Games</Category>
+      <Menu />
+      <StatusBar style="auto" />
+    </Container>
   );
 }
--- steam/src/screens/Profile/styles.js
+++ steam/src/screens/Profile/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components/native";
+import colors from "../../themes/colors";
 
 export const Container = styled.View`
   flex: 1;
@@ -9,82 +10,134 @@ export const Container = styled.View`
   padding-bottom: 50px;
 `;
 
-// export const Top = styled.View`
-// width: 390px;
-// height: 80px;
-// background: #202126;
-// align-items: center;
-// justify-content: center;
-// `;
-
 export const User = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 30px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 30px;
+  color: #ffffff;
 `;
 
 export const Name = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 18px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  color: #ffffff;
 `;
 
 export const Country = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 18px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  color: #ffffff;
 `;
 
 export const Level = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 32px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 32px;
+  color: #ffffff;
 `;
 
 export const Service = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 16px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 16px;
+  color: #ffffff;
+`;
+
+export const ViewButton = styled.View`
+
 `;
 
 export const EditButton = styled.TouchableOpacity`
-width: 192px;
-height: 42px;
-background: #2B3445;
-align-items: center;
-justify-content: center;
+  width: 192px;
+  height: 42px;
+  background: #2b3445;
+  align-items: center;
+  justify-content: center;
 `;
 
 export const TextButton = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 18px;
-color: #FFFFFF;
-text-align: center;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 18px;
+  color: #ffffff;
+  text-align: center;
 `;
 
 export const Status = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 24px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 24px;
+  color: #ffffff;
+  margin-top: 10px;
+  padding-left: 20px;
 `;
 
 export const LastView = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 20px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 20px;
+  color: #ffffff;
+  margin-top: 15px;
+  padding-left: 20px;
 `;
 
 export const Category = styled.Text`
-font-family: sans-serif;
-font-weight: 300;
-font-size: 20px;
-color: #FFFFFF;
+  font-family: sans-serif;
+  font-weight: 300;
+  font-size: 20px;
+  color: #ffffff;
+`;
+
+export const ViewStatus = styled.View`
+  width: 377px;
+  height: 93px;
+  margin: 0 0;
+  background: ${colors.secondary};
+`;
+
+export const ViewUser = styled.View`
+  padding-left: 15px;
+  flex-direction: row;
+  align-items: center;
+  width: 377px;
+  height: 55px;
+`;
+
+export const ButtonUser = styled.TouchableOpacity`
+  padding-left: 5px;
+`;
+
+export const ViewName = styled.View`
+  padding-left: 15px;
+  width: 377px;
+  height: 50px;
+  flex-direction: row;
+`;
+
+export const CountryIcon = styled.Image`
+width: 17.33px;
+height: 13px;
+margin-left: 10px;
+margin-right: 5px;
+`;
+
+export const ViewImage = styled.View`
+
+`;
+
+export const UserImage = styled.Image`
+width: 145px;
+height: 149px;
+margin-right: 20px;
+margin-left: -75px;
+`;
+
+export const ViewUserDetails = styled.View`
+justify-content: start;
+`;
+
+export const ViewUserComplete = styled.View`
+  flex-direction: row;
 `;
